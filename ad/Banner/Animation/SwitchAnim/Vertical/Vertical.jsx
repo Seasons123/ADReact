dd0989ac8be5efa5c7e76e7331ce6018343a9ebc
Seasons123 <1319075387@qq.com>
2017-05-12 13:39:20 +0800

    0512

--- ad/Banner/Animation/SwitchAnim/Vertical/Vertical.jsx
+++ ad/Banner/Animation/SwitchAnim/Vertical/Vertical.jsx
@@ -1,130 +1,67 @@
 import BannerAnim from '../../BannerAnim/BannerAnim';
 import QueueAnim from '../../QueueAnim/QueueAnim';
-import TweenOneAnim from '../../TweenOneAnim/TweenOneAnim';
+import TweenOneAnim from '../../TweenOneAnim/index';
 
 import React, { Component }  from 'react';
 
-import '../../../css/banner.css';
-import '../../../css/banner-anim.css';
+/*import '../../../css/banner.css';
+import '../../../css/banner-anim.css';*/
+
+import '../../../less/index.less';
+import '../../../less/index.less';
+import '../../../less/bgParallax.less';
 
 const { Element } = BannerAnim;
 const BgElement = Element.BgElement;
 export default class Vertical extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return (
-            <BannerAnim type="vertical">
-                <Element key="aaa"
-                         prefixCls="banner-user-elem"
-                >
-                    <BgElement
-                        key="bg"
-                        className="bg"
-                        style={{
-                            backgroundImage: 'url(https://os.alipayobjects.com/rmsportal/IhCNTqPpLeTNnwr.jpg)',
-                            backgroundSize: 'cover',
-                            backgroundPosition: 'center',
-                        }}
-                    />
-                    <QueueAnim name="QueueAnim">
-                        <h1 key="h1">Ant Motion Demo</h1>
-                        <p key="p">Ant Motion Demo.Ant Motion Demo.Ant Motion Demo.Ant Motion Demo</p>
-                    </QueueAnim>
-                    <TweenOneAnim animation={{ y: 50, opacity: 0, type: 'from', delay: 200 }} name="TweenOne">
-                        Ant Motion Demo.Ant Motion Demo
-                    </TweenOneAnim>
-                </Element>
-                <Element key="bbb"
-                         prefixCls="banner-user-elem"
-                >
-                    <BgElement
-                        key="bg"
-                        className="bg"
-                        style={{
-                            backgroundImage: 'url(https://os.alipayobjects.com/rmsportal/uaQVvDrCwryVlbb.jpg)',
-                            backgroundSize: 'cover',
-                            backgroundPosition: 'center',
-                        }}
-                    />
-                    <QueueAnim name="QueueAnim">
-                        <h1 key="h1">Ant Motion Demo</h1>
-                        <p key="p">Ant Motion Demo.Ant Motion Demo.Ant Motion Demo.Ant Motion Demo</p>
-                    </QueueAnim>
-                    <TweenOneAnim animation={{ y: 50, opacity: 0, type: 'from', delay: 200 }} name="TweenOne">
-                        Ant Motion Demo.Ant Motion Demo
-                    </TweenOneAnim>
-                </Element>
-            </BannerAnim>
+            <div  style={{ width:this.props.data.width, height:this.props.data.height,margin:'0 auto'}}>
+                <BannerAnim type="across">
+                    <Element key="aaa"
+                             prefixCls="banner-user-elem"
+                    >
+                        <BgElement
+                            key="bg"
+                            className="bg"
+                            style={{
+                                backgroundImage: 'url(https://os.alipayobjects.com/rmsportal/IhCNTqPpLeTNnwr.jpg)',
+                            }}
+                            scrollParallax={{ y: 100 }}
+                        />
+                        <QueueAnim name="QueueAnim">
+                            <h1 key="h1">Ant Motion Demo</h1>
+                            <p key="p">Ant Motion Demo.Ant Motion Demo.Ant Motion Demo.Ant Motion Demo</p>
+                        </QueueAnim>
+                        <TweenOneAnim animation={{ y: 50, opacity: 0, type: 'from', delay: 200 }} name="TweenOne">
+                            Ant Motion Demo.Ant Motion Demo
+                        </TweenOneAnim>
+                    </Element>
+                    <Element key="bbb"
+                             prefixCls="banner-user-elem"
+                    >
+                        <BgElement
+                            key="bg"
+                            className="bg"
+                            style={{
+                                backgroundImage: 'url(https://os.alipayobjects.com/rmsportal/uaQVvDrCwryVlbb.jpg)',
+                            }}
+                            scrollParallax={{ y: 100 }}
+                        />
+                        <QueueAnim name="QueueAnim">
+                            <h1 key="h1">Ant Motion Demo</h1>
+                            <p key="p">Ant Motion Demo.Ant Motion Demo.Ant Motion Demo.Ant Motion Demo</p>
+                        </QueueAnim>
+                        <TweenOneAnim animation={{ y: 50, opacity: 0, type: 'from', delay: 200 }} name="TweenOne">
+                            Ant Motion Demo.Ant Motion Demo
+                        </TweenOneAnim>
+                    </Element>
+                </BannerAnim>
+            </div>
         );
     }
 }
-
-/*
-const { Element } = BannerAnim;
-const BgElement = Element.BgElement;
-
-
-
-export default class Vertical extends React.Component {
-    render() {
-        return (
-            <BannerAnim type="vertical">
-                <Element key="element1" prefixCls="banner-user-elem" >
-                    <BgElement key="bg" className="bg "
-                               style={{
-                                   backgroundImage:'url(' + this.props.items[0].src + ')',
-                                   backgroundSize: 'cover',
-                                   backgroundPosition: 'center',
-                               }}
-                    />
-                    <div  className={this.props.items[0].textPosition} style={{color:this.props.items[0].textColor}}>
-                        <QueueAnim name="QueueAnim">
-                            <h1 key="h1">{this.props.items[0].textHeader}</h1>
-                        </QueueAnim>
-                        <QueueAnim name="QueueAnim" type="bottom">
-                            <p key="p1">{this.props.items[0].textOne}</p>
-                            <p key="p2">{this.props.items[0].textTwo}</p>
-                        </QueueAnim>
-                    </div>
-                </Element>
-                <Element key="element2" prefixCls="banner-user-elem" >
-                    <BgElement key="bg" className="bg"
-                               style={{
-                                   backgroundImage: 'url(' + this.props.items[1].src + ')',
-                                   backgroundSize: 'cover',
-                                   backgroundPosition: 'center',
-                               }}
-                    />
-                    <div  className={this.props.items[1].textPosition} style={{color: this.props.items[1].textColor}}>
-                        <QueueAnim name="QueueAnim">
-                            <h1 key="h1">{this.props.items[1].textHeader}</h1>
-                        </QueueAnim>
-                        <QueueAnim name="QueueAnim" type="bottom">
-                            <p key="p1">{this.props.items[1].textOne}</p>
-                            <p key="p2">{this.props.items[1].textTwo}</p>
-                        </QueueAnim>
-                    </div>
-                </Element>
-                <Element key="element3" prefixCls="banner-user-elem" >
-                    <BgElement key="bg" className="bg"
-                               style={{
-                                   backgroundImage: 'url(' + this.props.items[2].src + ')',
-                                   backgroundSize: 'cover',
-                                   backgroundPosition: 'center',
-                               }}
-                    />
-                    <div  className={this.props.items[2].textPosition} style={{color: this.props.items[2].textColor}}>
-                        <QueueAnim name="QueueAnim">
-                            <h1 key="h1">{this.props.items[2].textHeader}</h1>
-                        </QueueAnim>
-                        <QueueAnim name="QueueAnim" type="bottom">
-                            <p key="p1">{this.props.items[2].textOne}</p>
-                            <p key="p2">{this.props.items[2].textTwo}</p>
-                        </QueueAnim>
-                    </div>
-                </Element>
-            </BannerAnim>
-        );
-    }
-}
-*/
-
