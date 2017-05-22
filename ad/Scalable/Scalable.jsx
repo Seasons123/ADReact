a15265640da580b7a9629ca238439177a217793f
Seasons123 <1319075387@qq.com>
2017-05-22 17:21:29 +0800

    0522

--- ad/Scalable/Scalable.jsx
+++ ad/Scalable/Scalable.jsx
@@ -12,12 +12,13 @@ export default class Scalable extends Component {
             displaySmall: "block",
             displaySmallBottom:"none",
             closeDisplay:"block",
-            heightBigImage:this.props.items[1].height,
             widthBigImage:this.props.items[1].width,
-            animationTop: [{ y: '-1000px',opacity: 1, type: 'to', delay: this.props.delay,duration:this.props.duration }],
-            animationLeft: [{ x: '-2000px',opacity: 1, type: 'to', delay: this.props.delay,duration:this.props.duration }],
-            animationRight: [{ x: '2000px',opacity: 1, type: 'to', delay: this.props.delay,duration:this.props.duration }],
-            animationBottom:[{ y: '2000px',opacity: 1, type: 'to', delay: this.props.delay,duration:this.props.duration }],
+            heightBigImage:this.props.items[1].height,
+            duration:1600,
+            animationTop: [{ y: '-1000px',opacity: 1, type: 'to', delay: this.props.delay,duration:1600}],
+            animationLeft: [{ x: '-2000px',opacity: 1, type: 'to', delay: this.props.delay,duration:1600 }],
+            animationRight: [{ x: '2000px',opacity: 1, type: 'to', delay: this.props.delay,duration:1600 }],
+            animationBottom:[{ y: '1000px',opacity: 1, type: 'to', delay: this.props.delay,duration:1600*2 }],
         };
 
     }
@@ -30,10 +31,34 @@ export default class Scalable extends Component {
     // 开始计时自动消失
     autoDisappear() {
         if(this.props.autoDisappear) {
-            setTimeout(() => {
-                this.setState({heightBigImage: "0px"});
-                this.setState({displaySmallBottom: "block"});
-            }, this.props.delay *2);
+            if(this.props.position == "top" || this.props.position == "Top") {
+                setTimeout(() => {
+                    // 开始循环定时器
+                    let timer = setInterval(() => {
+                        // 获取元素总高度
+                        let totalHeight = parseInt(this.state.heightBigImage);
+                        // 定义一个变量保存元素当前高度
+                        let currentHeight = totalHeight;
+                        // 计算每次减去的值
+                        let decrement = totalHeight / (this.state.duration / 10);
+                        // 减去当前高度的一部分
+                        currentHeight = currentHeight - decrement;
+                        // 把当前高度赋值给height属性
+                        this.setState({heightBigImage: currentHeight + "px"});
+                        // 如果当前高度小于等于0，就关闭定时器
+                        if (currentHeight <= 0) {
+                            // 关闭定时器
+                            clearInterval(timer);
+                        }
+                    }, 10);
+
+            }, this.props.delay );
+           }else{
+                setTimeout(()=>{
+                    this.setState({heightBigImage: "0px"});
+                    this.setState({displaySmallBottom: "block"});
+                }, this.props.delay *2)
+            }
         }
     }
 
@@ -44,7 +69,7 @@ export default class Scalable extends Component {
     render() {
 
         let smallImageTop=(
-            <OneTween animation={[{ y: '-1000px' ,height:0, type: 'from', delay: this.props.delay *2,duration:this.props.duration }]}>
+            <OneTween animation={[{ y: '-1000px' ,height:0, type: 'from', delay: this.props.delay *2,duration:this.state.duration }]}>
                 <div  className="adSmall" style={{display:this.state.displaySmall}}>
                     <a href={this.props.link}
                        style={{backgroundImage:'url(' + this.props.items[0].src + ')',
@@ -61,8 +86,8 @@ export default class Scalable extends Component {
             </OneTween>
         );
         let smallImageBottom=(
-            <OneTween animation={[{ y: '1000px' , type: 'from', delay: this.props.delay *1.5,duration:this.props.duration }]}>
-                <div  className="adSmall" style={{display:this.state.displaySmallBottom}}>
+            <OneTween animation={[{ y: '1000px' , type: 'from', delay: this.props.delay *1.5,duration:this.state.duration }]}>
+                <div  className="adSmallBottom" style={{display:this.state.displaySmallBottom}}>
                     <a href={this.props.link}
                        style={{backgroundImage:'url(' + this.props.items[0].src + ')',
                            width: this.props.items[0].width,
@@ -79,15 +104,15 @@ export default class Scalable extends Component {
         );
         let smallImageLeft=(
             <div style={{ margin:'0 0'}}>
-                <OneTween animation={[{ x: '-100px' , type: 'from', delay: this.props.delay *2, duration:this.props.duration }]}>
-                    <div  className="adSmall" style={{display:this.state.displaySmall}}>
+                <OneTween animation={[{ x: '-100px' , type: 'from', delay: this.props.delay *2, duration:this.state.duration }]}>
+                    <div  className="adSmallLeft" style={{display:this.state.displaySmall}}>
                         <a href={this.props.link}
                            style={{backgroundImage:'url(' + this.props.items[2].src + ')',
                                width: this.props.items[2].width,
                                height:this.props.items[2].height,
                                display:this.state.displayBig}}>
                         </a>
-                        <a className="rightAd-close-smallImageLeft"
+                        <a className="rightAd-close-smallImageLeftRight"
                            style={{display:this.state.closeDisplay}}
                            onClick={this.handleCloseClick.bind(this)}
                            href="#">X
@@ -98,8 +123,8 @@ export default class Scalable extends Component {
         );
         let smallImageRight=(
             <div style={{ margin:'0 0',float:'right'}}>
-                <OneTween animation={[{ x: '2000' , type: 'from', delay: this.props.delay *2 ,duration:this.props.duration }]}>
-                    <div  className="adSmall" style={{display:this.state.displaySmall}}>
+                <OneTween animation={[{ x: '2000' , type: 'from', delay: this.props.delay *2 ,duration:this.state.duration }]}>
+                    <div  className="adSmallRight" style={{display:this.state.displaySmall}}>
                         <a href={this.props.link}
                            style={{backgroundImage:'url(' + this.props.items[2].src + ')',
                                width: this.props.items[2].width,
@@ -119,7 +144,7 @@ export default class Scalable extends Component {
         if( this.props.position == "top" || this.props.position == "Top" )
             return(
                 <div style={{ width:this.props.width,margin:'0 auto'}}>
-                    <OneTween animation={this.props.autoDisappear?this.state.animationTop:null}>
+                    <OneTween >
                         <div className="adBig" >
                             <a href={this.props.link}
                                style={{backgroundImage:'url(' + this.props.items[1].src + ')',
