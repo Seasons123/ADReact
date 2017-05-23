41d91ebd6fab44226345dd40769baa1dab61c674
Seasons123 <1319075387@qq.com>
2017-05-23 16:23:36 +0800

    0523

--- ad/Marquee/marquee.jsx
+++ ad/Marquee/marquee.jsx
@@ -53,4 +53,17 @@ Marquee.propsTypes = {
 	link:React.PropTypes.string
 };
 
+Marquee.defaultProps={
+    text: 'After all , tomorrow is another day ',
+    fontSize:'20px',
+    fontColor:'#323232',
+    fontFamily:'Microsoft YaHei',
+    link:"https://github.com/Seasons123",
+    autoRoll:true,
+    rollCount:2,
+    interval:20,
+    timingFunction:'linear',
+    direction:'left'
+};
+
 export default Marquee;
