9f78b7b1d45df52bd35aea8da1752d4e6335ec1d
seasons123 <peichunyansd@gmail.com>
2018-05-17 22:48:08 +0800

    0517

--- ad/MainPage.jsx
+++ ad/MainPage.jsx
@@ -1,19 +1,54 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Interactive from './Interactive/Interactive';
+import Banner from './Banner/Banner';
+
+
 
 const IMAGE_DATA = [
-    {src: require('./assets/svg/demo.svg')},
-    {src: require('./assets/svg/reactLogo.svg')}
+    {
+        src: require('./assets/images/banner/size5(960.380)/1.jpg'),
+        alt: 'images-1',
+        textHeader:'Banner animation demo',
+        textOne:'Today is not another day ,today I will create something beautiful ',
+        textTwo:'Study hard , play harder',
+        textColor:'#323232',
+        textPosition:'upLeft'
+    },
+    {
+        src: require('./assets/images/banner/size5(960.380)/2.jpg'),
+        alt: 'images-2',
+    },
+    {
+        src: require('./assets/images/banner/size5(960.380)/3.jpg'),
+        alt: 'images-3',
+        textHeader:'Banner animation demo picture 3',
+        textOne:'Today is not another day ,today I will create something beautiful ',
+        textTwo:'Study hard , play harder',
+        textColor:'#323232',
+        textPosition:'upRight'
+    },
+    {
+        src: require('./assets/video/YUNDI.mp4'),
+    }
 ];
 
 var MainPage=React.createClass({
     render:function(){
 
         return(
-            <Interactive
+            <Banner
                 items={IMAGE_DATA}
-                type={'graphFocus'}
+                width={'960px'}
+                height={'380px'}
+                delay={0}
+                duration={1450}
+                autoPlaySpeed={5000}
+                autoPlay={true}
+                pause={false}
+                arrow={true}
+                arrowDefault={true}
+                thumb={true}
+                animType={"vertical"}
             />
         );
     },
