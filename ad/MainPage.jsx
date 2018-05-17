03918d027f9f387a0e22c6c566249692d8fd26a1
seasons123 <peichunyansd@gmail.com>
2018-05-17 19:51:55 +0800

    0517

--- ad/MainPage.jsx
+++ ad/MainPage.jsx
@@ -1,54 +1,28 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Banner from './Banner/Banner';
-
-
+import PopUp from './PopUp/PopUp';
 
 const IMAGE_DATA = [
-    {
-        src: require('./assets/images/banner/size5(960.380)/1.jpg'),
-        alt: 'images-1',
-        textHeader:'Banner animation demo',
-        textOne:'Today is not another day ,today I will create something beautiful ',
-        textTwo:'Study hard , play harder',
-        textColor:'#323232',
-        textPosition:'upLeft'
-    },
-    {
-        src: require('./assets/images/banner/size5(960.380)/2.jpg'),
-        alt: 'images-2',
-    },
-    {
-        src: require('./assets/images/banner/size5(960.380)/3.jpg'),
-        alt: 'images-3',
-        textHeader:'Banner animation demo picture 3',
-        textOne:'Today is not another day ,today I will create something beautiful ',
-        textTwo:'Study hard , play harder',
-        textColor:'#323232',
-        textPosition:'upRight'
-    },
-    {
-        src: require('./assets/video/YUNDI.mp4'),
-    }
+    {src: require('./assets/images/popup/size2(800.450)/demo1.jpg')},
+    {src: require('./assets/images/popup/couplet/couplet1.jpg')},
+    {src: require('./assets/images/popup/couplet/couplet2.jpg')}
 ];
+
+
 
 var MainPage=React.createClass({
     render:function(){
 
         return(
-            <Banner
+            <PopUp
                 items={IMAGE_DATA}
-                width={'960px'}
-                height={'380px'}
-                delay={0}
-                duration={1450}
-                autoPlaySpeed={5000}
-                autoPlay={true}
-                pause={false}
-                arrow={true}
-                arrowDefault={true}
-                thumb={true}
-                animType={"vertical"}
+                width={'100px'}
+                height={'360px'}
+                link={"https://github.com"}
+                position={"couplet"}
+                distanceX={"60px"}
+                distanceY={"110px"}
+                mask={false}
             />
         );
     },
--- ad/PopUp/example/couplet/MainPage.jsx
+++ ad/PopUp/example/couplet/MainPage.jsx
@@ -1,38 +1,31 @@
 import React from 'react';
 import { render } from 'react-dom';
-
 import PopUp from './PopUp/PopUp';
 
 const IMAGE_DATA = [
-    {
-        src: require('./assets/images/popup/size2(1440)(800.450)/demo1.jpg')
-    },
-    {
-        src: require('./assets/images/popup/couplet/couplet1.jpg')
-    },
-    {
-        src: require('./assets/images/popup/couplet/couplet2.jpg')
-    }
+    {src: require('./assets/images/popup/size2(800.450)/demo1.jpg')},
+    {src: require('./assets/images/popup/couplet/couplet1.jpg')},
+    {src: require('./assets/images/popup/couplet/couplet2.jpg')}
 ];
 
 
 
 var MainPage=React.createClass({
-  render:function(){
+    render:function(){
 
-    return(
-        <PopUp
-            items={IMAGE_DATA}
-            width={'100px'}
-            height={'360px'}
-            link={"https://github.com"}
-            position={"couplet"}
-            distanceX={"60px"}
-            distanceY={"110px"}
-            mask={false}
-        />
-    );
-  },
+        return(
+            <PopUp
+                items={IMAGE_DATA}
+                width={'100px'}
+                height={'360px'}
+                link={"https://github.com"}
+                position={"couplet"}
+                distanceX={"60px"}
+                distanceY={"110px"}
+                mask={false}
+            />
+        );
+    },
 });
 module.exports=MainPage;
 
